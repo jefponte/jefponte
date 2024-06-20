c0233788fbec7999fc1c431ef6cc6bf368e79d75
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-06-20 16:26:59 -0300

    Test cicd

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-<html>
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
-
-    <p>OLa, eu vim de uma github action</p>
+    <h1>Morgana eu te amo</h1>
 </body>
 </html>
