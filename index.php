e46880f1be595d262a706bdfff138d7cadf46f64
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-07-22 18:25:23 -0300

    Test flickr

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Ola, mundo</h1>
+    <a data-flickr-embed="true" href="https://www.flickr.com/photos/jefponte/albums/72177720319042023" title="Melhores fotos"><img src="https://live.staticflickr.com/65535/53775904660_4f32dbc2ee_z.jpg" width="640" height="480" alt="Melhores fotos"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
 </body>
 </html>
